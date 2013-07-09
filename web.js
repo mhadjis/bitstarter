df42528085e8d2a11cd7e04e081dfc67beacfd0a
hadjistratis emmanuel <m_hadjis@yahoo.fr>
2013-07-09 18:37:06 +0000

    xx

--- web.js
+++ web.js
@@ -1,9 +1,10 @@
 var express = require('express');
-
+var fs=require("fs");
+var content=fs.readFileSync("index.html");
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World! zzzzzzzzzqqqqqzzzzzzzzzzzz 2');
+  response.send(content);
 });
 
 var port = process.env.PORT || 5000;
