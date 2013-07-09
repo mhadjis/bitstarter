b90eb40cdcffdfb37eaedef54700e024fc91d640
hadjistratis emmanuel <m_hadjis@yahoo.fr>
2013-07-09 18:42:30 +0000

    a

--- web.js
+++ web.js
@@ -1,6 +1,6 @@
 var express = require('express');
-var fs=require("fs");
-var content=fs.readFileSync("index.html");
+var fs=require('fs');
+var content=fs.readFileSync('index.html','utf8');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
